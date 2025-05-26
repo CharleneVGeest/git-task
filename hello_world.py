52fb532cdadef326340545005fd06c9b007b624e
CharleneVGeest <charlenetedman@gmail.com>
2025-05-26 19:19:59 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+echo 'console.log("Hello World!");' > hello_world.js
+git status
+
+git add hello_world.py
+
+print("Git is awesome!")
+
+git status
+
+git add hello_world.py
+
+git status
+
+git commit -m "Updated message to say 'Git is awesome!'"
+
+git status
+
